b527d80b747d2003fee4ce73acc2496b810b8a81
Luigi Mendoza <ludk17@gmail.com>
2023-04-18 09:13:52 -0500

    add intiantate

---
+++ vjupn-unity/Assets/Scripts/BalaController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaController : MonoBehaviour
+{
+    public float velocityX = 0.1f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(this.gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(velocityX, rb.velocity.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("trigger");
+        Debug.Log(other.gameObject.name);
+        Destroy(this.gameObject);
+    }
+}
--- vjupn-unity/Assets/Scripts/PlayerController.cs
+++ vjupn-unity/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float xVelocity = 10f;
-    
+    public GameObject bala;
     private Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        var yVelocity = rb.velocity.y;
-        
-        rb.velocity = new Vector2(0, yVelocity);
-        
-        if(Input.GetKey(KeyCode.RightArrow))
-            rb.velocity = new Vector2(xVelocity, yVelocity);
-        if(Input.GetKey(KeyCode.LeftArrow))
-            rb.velocity = new Vector2(-xVelocity, yVelocity);
+       if(Input.GetKeyUp(KeyCode.U)) {
+         // crear a la bala
+         // que objeto
+         // en donde debe aparecer
+         // que rotacion va a tener?
+         var currentPosition = transform.position;
+         var position = new Vector3(currentPosition.x - 5, currentPosition.y, 10);
+         var balaGO = Instantiate(bala, position, Quaternion.identity);
+         var controller = balaGO.GetComponent<BalaController>();
+         controller.velocityX = -1f;
+       }
     }
 }
